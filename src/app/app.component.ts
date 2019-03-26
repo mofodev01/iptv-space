5a1e92e02eccd3455c3b6eae7641bbedf19cf8f2
mustapha <mofodev01@gmail.com>
2019-03-26 20:31:52 +0100

    change x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -70,15 +70,13 @@ export class MyApp {
     this.initializeApp();
    /// this.showBanner();
     this.localisation();
-   // this.fetchuser();
+    this.fetchuser();
     // used for an example of ngFor and navigation   SeriesPage
    
 
   }
 
-  ionViewWillEnter(){
-   this.fetchuser();
-  }
+  
 
   fetchuser(){
   
